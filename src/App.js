f2b85481a1fc35dfbc01cfe1f9b6863c50d449dd
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2022-05-01 22:22:21 -0500

    add axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get('/api/getTest')
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  }, []);
   return (
     <div className="App">
-      <div>Hello </div>
+      <div>{data && data}</div>
+      <div>{loading && 'Loading...'}</div>
     </div>
   );
 }
