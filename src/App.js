08cb6f9558abb26fac7faa9ae12d0b29cae67bfb
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2022-05-02 09:18:18 -0500

    add google api

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import axios from 'axios';
 import { useQuery } from 'react-query';
 
 function App() {
-  const { isLoading, error, data, isFetching } = useQuery('getTest', () =>
-    axios.get('/api/getTest').then((res) => res.data)
+  const { isLoading, error, data } = useQuery('getPlace', () =>
+    axios.get('/api/getPlace').then((res) => res.data)
   );
   return (
     <div className="App">
       <div>{data && data}</div>
-      <div>{isLoading && 'Loading...'}~</div>
+      <div>{isLoading && 'Loading...'}</div>
+      <div>{error && error}</div>
     </div>
   );
 }
